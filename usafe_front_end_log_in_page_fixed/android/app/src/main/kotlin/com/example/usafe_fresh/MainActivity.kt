de5eb2cc099aa2e62fe55668ea8f7a2ebffe7964
Sanuka Pathiraja <sanukapathiraja@gmail.com>
2026-02-07 01:29:49 +0530

    sync workspace state

--- usafe_front_end_log_in_page_fixed/android/app/src/main/kotlin/com/example/usafe_fresh/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.usafe_fresh
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- usafe_front_end_log_in_page_fixed/android/app/src/main/kotlin/com/example/usafe_front_end_log_in_page_fixed/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.usafe_front_end_log_in_page_fixed
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
